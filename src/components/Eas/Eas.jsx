2b9791daf689008c0718e2134a17707c25909b5a
Denis <denis-sergeev-85@inbox.ru>
2024-05-31 11:20:14 +0300

    исправлено

--- src/components/Eas/Eas.jsx
+++ src/components/Eas/Eas.jsx
@@ -2,7 +2,11 @@ import CryptoJS from "crypto-js";
 import { useState } from "react";
 
 function Eas() {
+  let key = CryptoJS.enc.Latin1.parse("1234567812345678");
+  let iv = CryptoJS.enc.Latin1.parse("1234567812345678");
+
   const [textareaText, setTextareaText] = useState("");
+  const [decrareaText, setDecrareaText] = useState("");
   const [encrypt, setEncrypt] = useState("");
   const [decrypt, setDecrypt] = useState("");
 
@@ -10,26 +14,32 @@ function Eas() {
     setTextareaText(event.target.value);
   }
 
-  function encryptFun() {
-    let data = textareaText;
-    let key = CryptoJS.enc.Latin1.parse("1234567812345678");
-    let iv = CryptoJS.enc.Latin1.parse("1234567812345678");
-    let encrypted = CryptoJS.AES.encrypt(data, key, {
-      iv: iv,
-      mode: CryptoJS.mode.CBC,
-      padding: CryptoJS.pad.ZeroPadding,
-    });
-    setEncrypt("" + encrypted);
-    let decrypted = CryptoJS.AES.decrypt(encrypted, key, {
+  function AesDeCr(event) {
+    setDecrareaText(event.target.value);
+    console.log(decrareaText);
+  }
+
+  function decryptFun() {
+    let decrypted = CryptoJS.AES.decrypt(decrareaText, key, {
       iv: iv,
       padding: CryptoJS.pad.ZeroPadding,
     });
     setDecrypt("" + decrypted.toString(CryptoJS.enc.Utf8));
   }
 
+  function encryptFun() {
+    let data = textareaText;
+    let encrypted = CryptoJS.AES.encrypt(data, key, {
+      iv: iv,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.ZeroPadding,
+    });
+    setEncrypt("" + encrypted);
+  }
+
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <h2>Шифрование/расшифрование алгоритмом aes</h2>
+      <h2>Шифрование алгоритмом aes</h2>
       <textarea
         onChange={AesChange}
         type="text"
@@ -38,18 +48,26 @@ function Eas() {
       ></textarea>
       <button onClick={encryptFun}>получить Eas</button>
       <div style={{ marginTop: "20px" }}>
-        <div>
-          <span>
-            <b>Зашифровка: </b>
-            {encrypt}
-          </span>
-        </div>
-        <div>
-          <span>
-            <b>Расшифровка: </b>
-            {decrypt}
-          </span>
-        </div>
+        <span>
+          <b>Зашифровано: </b>
+          {encrypt}
+        </span>
+      </div>
+
+      <h2>Расшифрование алгоритмом aes</h2>
+      <textarea
+        onChange={AesDeCr}
+        type="text"
+        style={{ marginBottom: "20px" }}
+        placeholder="Введите значение поля"
+      ></textarea>
+      <button onClick={decryptFun}>Расшифровать</button>
+
+      <div style={{ marginTop: "20px" }}>
+        <span>
+          <b>Расшифровка: </b>
+          {decrypt}
+        </span>
       </div>
     </div>
   );
